2fd8491aca80a66f5a2f1e964355480d3bc4f332
Francinei Gomes de Morais <fgomesm@yahoo.com.br>
2018-08-18 19:14:57 -0300

    Update bot.py

--- codes/bot.py
+++ codes/bot.py
@@ -7,21 +7,22 @@ Created on Sat Aug 18 14:11:12 2018
 import telepot
 import scrapReddit as sReddit
 
-min_ups = 100
-max_limit = 10
+min_ups = 1000
+max_limit = 300
 
+bot_token = "683547744:AAH0MpdLcXPJV6URmeZZP4iJv0UXG4jVIZ8"
+bot = telepot.Bot(bot_token)
 
-bot = telepot.Bot("683547744:AAH0MpdLcXPJV6URmeZZP4iJv0UXG4jVIZ8")
-# Receiving messages
+# receiving messages
 input_command  = bot.getUpdates() 
 
 # getting chat id
 chat_id = input_command[len(input_command)-1]['message']['chat']['id']
 
+# getting message text
 msg_received = input_command[len(input_command)-1]['message']['text']
 msg_received_list = msg_received.split()
 
-#%%
 if msg_received_list[0] in '/NadaPraFazer':
     # get subreddit names 
     subreddit_names = msg_received_list[1].split(';')
@@ -29,6 +30,4 @@ if msg_received_list[0] in '/NadaPraFazer':
 for subreddit_name in subreddit_names:
     get_document = sReddit.reddit_list(subreddit_name, min_ups, max_limit)
     bot.sendMessage(chat_id,get_document)
-    # print(get_document)
-    
-#%%
+ 
