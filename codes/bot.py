5f8fa6dca246a0158c75f49da7330164dc39ebec
Francinei Gomes de Morais <fgomesm@yahoo.com.br>
2018-08-19 22:01:48 -0300

    Update bot.py

--- codes/bot.py
+++ codes/bot.py
@@ -7,8 +7,8 @@ Created on Sat Aug 18 14:11:12 2018
 import telepot
 import scrapReddit as sReddit
 
-min_ups = 1000
-max_limit = 300
+min_ups = 5000
+max_limit = 100
 
 bot_token = "683547744:AAH0MpdLcXPJV6URmeZZP4iJv0UXG4jVIZ8"
 bot = telepot.Bot(bot_token)
